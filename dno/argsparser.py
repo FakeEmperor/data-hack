2b70231e95dc67ca3e2e153ecfd940f333af2391
FakeEmperor <>
2019-03-21 01:34:28 +0300

    Better help message

--- dno/argsparser.py
+++ dno/argsparser.py
@@ -3,24 +3,29 @@ import argparse
 
 class ArgsParser:
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('X', type=float, help='X destanation coordinate')
-        self.parser.add_argument('Y', type=float, help='Y destanation coordinate')
-        self.parser.add_argument('Z', type=float, help='Z destanation coordinate')
+        self.parser = argparse.ArgumentParser(
+            description="This solver can solve numerically, using differential equations,"
+                        "a high altitude precision drop. "
+                        "It takes into account mass, different winds, "
+                        "initial object's velocity and aerodynamic forces to provide accurate results."
+                        "Results are validated using high precision simulation.")
+        self.parser.add_argument('X', type=float, help='X destination coordinate')
+        self.parser.add_argument('Y', type=float, help='Y destination coordinate')
+        self.parser.add_argument('Z', type=float, help='Z destination coordinate')
         self.parser.add_argument('-H', dest='H0', type=float,
-                                 help='High of drop', default=1000)
+                                 help='Drop altitude, m', default=1000)
         self.parser.add_argument('-V', dest='V0', type=float,
-                                 help='Started velocity of drop', default=250)
+                                 help='Initial velocity in XZ space, m/s', default=250)
         self.parser.add_argument('-F', dest='F_path', type=str,
-                                 help='path to .csv file data with F info', default="data\\F.csv")
+                                 help='Path to a CSV file data with F info', default="data\\F.csv")
         self.parser.add_argument('-W', dest='W_path', type=str,
-                                 help='path to .csv file with winds info', default="data\\Wind.csv")
+                                 help='path to a CSV file with winds info', default="data\\Wind.csv")
         self.parser.add_argument('-a', dest='alpha', type=float,
-                                 help='angle of drop in radians', default=0)
+                                 help='Velocity angle, grads', default=0)
         self.parser.add_argument('-m', dest='m', type=float,
-                                 help='mass of object', default=100)
+                                 help='Object\'s mass, kg', default=100)
         self.parser.add_argument('-v', dest='v', type=int,
-                                 help='verbosity level', default=50)
+                                 help='Verbosity level for model simulations', default=50)
 
     def getParser(self):
         return self.parser.parse_args()
